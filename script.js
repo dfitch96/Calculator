c6bc90df17f2ec680646dd9434664d813528f918
Dylan Fitch <kkarl30@gmail.com>
2024-12-04 19:35:32 -0500

    operate

--- script.js
+++ script.js
@@ -7,6 +7,27 @@ const DIV = '/';
 const POW = '^';
 const FACT = '!';
 
+
+function operate(operator, op1, op2){
+
+    switch(operator){
+        case ADD:
+            return add(op1, op2);
+        case SUB:
+            return subtract(op1, op2);
+        case MUL:
+            return multiply(op1, op2);
+        case DIV:
+            return divide(op1, op2);
+        case POW:
+            return power(op1, op2);
+    }
+
+
+}
+
+
+
 function add (a=0, b=0) {
     return a + b;
 };
